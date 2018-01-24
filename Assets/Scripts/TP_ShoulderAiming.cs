cdcd219b75987c4e88a071093b5d5e6f0d3138c4
charlotteAuger <auger.cha@gmail.com>
2018-01-24 15:56:46 +0100

    TPA : Aiming Marker movement

--- Assets/Scripts/TP_ShoulderAiming.cs
+++ Assets/Scripts/TP_ShoulderAiming.cs
@@ -7,6 +7,12 @@ public class TP_ShoulderAiming : MonoBehaviour {
 	public Transform aimMarker;
 	public CinemachineVirtualCamera aimingView;
 	public CinemachineFreeLook normalView;
+
+	Vector3[] cameraCorners;
+	public float aimMarkerSpeed;
+
+
+
 	// Use this for initialization
 	void Start () {
 		
@@ -14,10 +20,36 @@ public class TP_ShoulderAiming : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		/*if (Input.GetButtonDown ("Hack")) 
+		/*if (Input.GetAxis ("OrbitHorizontal") > 0.2f) 
 		{
-			GetTargetPoint ();
+			aimMarker.localPosition = new Vector3 (aimMarker.localPosition.x+aimMarkerSpeed, aimMarker.localPosition.y, aimMarker.localPosition.z);
+		}
+
+		if (Input.GetAxis ("OrbitHorizontal") < -0.2f) 
+		{
+			aimMarker.localPosition = new Vector3 (aimMarker.localPosition.x-aimMarkerSpeed, aimMarker.localPosition.y, aimMarker.localPosition.z);
+		}
+
+		if (Input.GetAxis ("OrbitVertical") > 0.2f) 
+		{
+			aimMarker.localPosition = new Vector3 (aimMarker.localPosition.x, aimMarker.localPosition.y-aimMarkerSpeed, aimMarker.localPosition.z);
+		}
+
+		if (Input.GetAxis ("OrbitVertical") < -0.2f) 
+		{
+			aimMarker.localPosition = new Vector3 (aimMarker.localPosition.x, aimMarker.localPosition.y+aimMarkerSpeed, aimMarker.localPosition.z);
 		}*/
+
+		if (Input.GetAxis ("OrbitHorizontal") > 0.2f || Input.GetAxis ("OrbitHorizontal") < -0.2f || Input.GetAxis ("OrbitVertical") > 0.2f || Input.GetAxis ("OrbitVertical") < -0.2f) 
+		{
+			float vChange = -Input.GetAxis("OrbitVertical")*aimMarkerSpeed;
+			float hChange = Input.GetAxis("OrbitHorizontal")*aimMarkerSpeed;
+
+			aimMarker.localPosition = new Vector3 (aimMarker.localPosition.x+hChange, aimMarker.localPosition.y+vChange, aimMarker.localPosition.z);
+		}
+
+
+		UpdateCameraClipPoints (transform.position, Camera.main.transform.rotation, cameraCorners);
 	}
 
 	public void AimingMode (bool value)
@@ -37,4 +69,25 @@ public class TP_ShoulderAiming : MonoBehaviour {
 		}
 		return aimingPoint;
 	}
+
+	public void UpdateCameraClipPoints(Vector3 cameraPosition, Quaternion atRotation, Vector3[] intoArray)
+	{
+		//clear the content of intoArray
+		intoArray = new Vector3[4];
+
+		float z = Camera.main.nearClipPlane;
+		float x = Mathf.Tan (Camera.main.fieldOfView / 3.41f) * z;
+		float y = x / Camera.main.aspect;
+
+		//top left
+		intoArray[0] = (atRotation * new Vector3(-x,y,z))+ cameraPosition; //added and rotated the point relative to camera
+		//top right
+		intoArray[1] = (atRotation * new Vector3(x,y,z))+ cameraPosition;
+		//bottom left
+		intoArray[2] = (atRotation * new Vector3(-x,-y,z))+ cameraPosition;
+		//bottom right
+		intoArray[3] = (atRotation * new Vector3(x,-y,z))+ cameraPosition;
+	}
+
+
 }
