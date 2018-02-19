15cb6108862b47d12141892ac97a6b3e84db502b
Cramena <allezcheznice@gmail.com>
2018-02-19 16:57:12 +0100

    Search state reworked

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
-enum EnemyState {
+public enum EnemyState {
 	patrol,
 	chase,
 	search
@@ -12,10 +12,12 @@ enum EnemyState {
 
 public class EnemyController : MonoBehaviour {
 
-	EnemyState state = EnemyState.patrol;
+	public EnemyState state = EnemyState.patrol;
 	public Transform[] patrolPoints;
+	public Transform[] searchPatrolPoints;
+	public LevelManager level;
+
 	private NavMeshAgent agent;
-	public int currentDestination;
 	private int destination;
 	private Plane[] cameraPlanes;
 	public Camera view;
@@ -24,11 +26,12 @@ public class EnemyController : MonoBehaviour {
 	float angleDifference;
 	RaycastHit hit;
 
-	float searchTimer;
 
 	// Use this for initialization
 	void Start ()
 	{
+		print (searchPatrolPoints.Length);
+		level = GameObject.Find ("LevelManager").GetComponent<LevelManager> ();
 		player = GameObject.Find ("Player").GetComponent<Collider>();
 		view = GetComponentInChildren<Camera> ();
 		cameraPlanes = GeometryUtility.CalculateFrustumPlanes (view);
@@ -40,19 +43,14 @@ public class EnemyController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-		print (destination);
 		Debug.DrawRay (transform.position, transform.forward, Color.red, 1f);
 		angle = (transform.position - player.transform.position).normalized;
 		angleDifference = Vector3.Angle (-transform.forward, angle);
 
-		switch (state) {
+		switch (state)
+		{
 		case EnemyState.chase:
-			if (angleDifference >= 30) {
-				state = EnemyState.search;
-				searchTimer = 1f;
-			} else if (Vector3.Distance (transform.position, player.transform.position) < 1) {
-				Attack ();
-			}
+			ChaseBehavior ();
 			break;
 
 		case EnemyState.patrol:
@@ -60,87 +58,135 @@ public class EnemyController : MonoBehaviour {
 			break;
 
 		case EnemyState.search:
+			SearchPatrol ();
 			CheckForPlayer ();
-			WaitBeforePatrol ();
 			break;
 		}
 
 	}
+		
+	void ChaseBehavior()
+	{
+		if (angleDifference >= 30)
+		{
+			StartSearch ();
+		}
+		else if (Vector3.Distance (transform.position, player.transform.position) < 1)
+		{
+			Attack ();
+		}
+	}
+
+	void SearchPatrol()
+	{
+		
+	}
 
 	void OnTriggerEnter (Collider other)
 	{
-		if (state == EnemyState.patrol)
+		if (other.tag == "PatrolPoint" && other.transform == patrolPoints[destination])
 		{
-			if (other.tag == "PatrolPoint" && other.transform == patrolPoints[destination])
+			if (state == EnemyState.patrol)
 			{
-				SwitchDestination ();
+				SwitchDestination (patrolPoints);
+			}
+			else if (state == EnemyState.search)
+			{
+				SwitchDestination (searchPatrolPoints);
+				CheckEndSearch (other);
 			}
 		}
 	}
-
-	void Attack () {
+		
+	void Attack ()
+	{
 		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
 	}
 
-	void WaitBeforePatrol () {
-		searchTimer -= Time.deltaTime;
-		print (searchTimer);
-		if (searchTimer <= 0) {
-			state = EnemyState.patrol;
+	void CheckEndSearch (Collider _other)
+	{
+		if (_other.transform == searchPatrolPoints[3])
+		{
 			StartPatrol ();
 		}
 	}
-
-	void CheckForPlayer() {
+		
+	void CheckForPlayer()
+	{
 		if (angleDifference < 30 && Physics.Linecast (transform.position, player.transform.position, out hit)) {
-			if (hit.collider == player) {
-				state = EnemyState.chase;
-				StartCoroutine ("GetPlayerPosition");
+			if (hit.collider == player)
+			{
+				StartChase ();
 			}
 		}
 	}
 
-	IEnumerator GetPlayerPosition () {
-		if (state == EnemyState.chase) {
+	IEnumerator GetPlayerPosition ()
+	{
+		if (state == EnemyState.chase)
+		{
 			agent.destination = player.transform.position;
 			yield return new WaitForSeconds (.2f);
 			StartCoroutine ("GetPlayerPosition");
 		}
 	}
 
-	void StartPatrol() {
+	void StartSearch()
+	{
+		state = EnemyState.search;
+		agent.speed = 2f;
+		GetSearchPoints ();
+		destination = 0;
+		agent.destination = searchPatrolPoints [0].position;
+	}
+
+	void StartChase()
+	{
+		state = EnemyState.chase;
+		agent.speed = 7f;
+		StartCoroutine ("GetPlayerPosition");
+	}
+		
+	void StartPatrol()
+	{
+		state = EnemyState.patrol;
+		agent.speed = 3.5f;
 		if (patrolPoints.Length > 0)
 		{
 			agent.destination = patrolPoints [0].position;
-			currentDestination = 0;
 			destination = 0;
 		}
 	}
-
-	void SwitchDestination()
+		
+	void GetSearchPoints()
 	{
-		print ("Reached destination");
+		searchPatrolPoints = new Transform[] {level.searchPoints [0], level.searchPoints [0], level.searchPoints [0], level.searchPoints [0]};
+		print (searchPatrolPoints.Length);
+		for (int i = 0; i < 3; i++)
+		{
+			searchPatrolPoints[i] = level.searchPoints [0];
+			for (int x = 0; x < level.searchPoints.Length; x++)
+			{
+				if (Vector3.Distance (transform.position, level.searchPoints[x].position) < Vector3.Distance (transform.position, searchPatrolPoints[i].position))
+				{
+					searchPatrolPoints [i] = level.searchPoints [x];
+				}
+			}
+		}
 
-
-		if (destination < patrolPoints.Length-1)
+	}
+		
+	void SwitchDestination(Transform[] _patrolPoints)
+	{
+		if (destination < _patrolPoints.Length-1)
 		{
 			destination++;
 		}
 		else
 		{
-			
 			destination = 0;
 		}
 
-		if (currentDestination < patrolPoints.Length-1)
-		{
-			currentDestination++;
-		}
-		else
-		{
-			currentDestination = 0;
-		}
-
-		agent.destination = patrolPoints [destination].position;
+		agent.destination = _patrolPoints [destination].position;
 	}
 }
