de72eaa76178a391dc5e1e5d6bd6d47601c7a767
charlotteAuger <auger.cha@gmail.com>
2018-03-25 15:47:22 +0200

    LD trap + fixed prefabs

--- Assets/Scripts/Hack/H_Trap.cs
+++ Assets/Scripts/Hack/H_Trap.cs
@@ -2,15 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class H_Trap : Hackable {
+public class H_Trap : Hackable 
+{
+	public float openTimer;
+	public Transform openTarget;
+
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	public override void Switch()
+	{
+		if (!isActivated) 
+		{
+			isActivated = true;
+			OnActivationEffect ();
+		}
+	}
+
+	public override void OnActivationEffect()
+	{
+		StartCoroutine (ActivatedTrap());
+	}
+
+	IEnumerator ActivatedTrap()
+	{
+		Vector3 targetPosition = openTarget.position;
+		Vector3 startPosition = transform.position;
+
+		for (float i = 0; i <= 1f; i += Time.deltaTime*2) 
+		{
+			transform.position = Vector3.Lerp (startPosition, targetPosition, i);
+			yield return null;
+		}
+
+		yield return new WaitForSeconds(openTimer);
+
+		for (float i = 0; i <= 1f; i += Time.deltaTime*2) 
+		{
+			transform.position = Vector3.Lerp (targetPosition, startPosition, i);
+			yield return null;
+		}
+
+		isActivated = false;
 	}
 }
