16dca60b5edfba1d5628858154b139200747392a
charlotteAuger <auger.cha@gmail.com>
2018-05-30 10:35:15 +0200

    Level transition

--- Assets/Scripts/Hack/HackSwitch.cs
+++ Assets/Scripts/Hack/HackSwitch.cs
@@ -1,10 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HackSwitch : MonoBehaviour {
 
 	public Hackable[] targets;
+    public Image icon;
+    public Sprite[] icons;
+    bool init;
+
+    void Update()
+    {
+        if (!init)
+        {
+            if (targets.Length > 0)
+                icon.sprite = icons[targets[0].icon];
+            init = true;
+        }
+    }
 
 	public void SwitchTargets()
 	{
